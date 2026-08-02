1300cff4034e02aea1cf066498af35da03943468
root <root@ser178557585697.local>
2026-08-02 14:29:08 +0000

    发布 1.7.59

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,8 +40,8 @@ android {
         applicationId = "top.asdb.codexremote"
         minSdk = 26
         targetSdk = 34
-        versionCode = 80
-        versionName = "1.7.58"
+        versionCode = 81
+        versionName = "1.7.59"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
